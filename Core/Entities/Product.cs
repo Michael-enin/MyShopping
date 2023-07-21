1ab3e2e5201b2857065603746e50a451323d42bc
MichaelGobena <MGobena@excellerentsolutions.com>
2023-07-21 14:56:36 +0300

    cleaning the new arrchitecture

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,13 +1,16 @@
+
 namespace Core.Entities
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public Product(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
-        public int Id { get; set; }
+
         public string? Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string pictureUrl { get; set; } = string.Empty;
+        public ProductType ProductType { get; set; }
+        public int productTypeId { get; set; }
+        public ProductBrand ProductBrand { get; set; }
+        public int productBrandId { get; set; }
     }
 }
---
+++ Infrastructure/DataStore/EntityConfguration/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Core.Entities;
+namespace Infrastructure.DataStore.EntityConfguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Id).IsRequired();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Description).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+            builder.HasOne(x => x.ProductBrand).WithMany()
+                          .HasForeignKey(x => x.productBrandId);
+            builder.HasOne(x => x.ProductType).WithMany()
+            .HasForeignKey(x => x.productTypeId);
+
+        }
+    }
+}
